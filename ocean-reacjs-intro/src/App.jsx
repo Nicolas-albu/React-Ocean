5788593f93c732bcc24d30b7bd8cca2fe40d46cd
Nicolas Albuquerque R <nicolaalbuquerque581@gmail.com>
2023-04-28 17:09:32 -0300

    initial update

---
+++ ocean-reacjs-intro/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Samsung Ocean</h1>
+      <div>
+        <h2>Pikachu</h2>
+        <img 
+        width="200"
+        src="https://sm.ign.com/ign_br/screenshot/default/025pikachu-os-anime-10_5z4v.png" />
+      </div>
+    </>
+  )
+}
+
+export default App
